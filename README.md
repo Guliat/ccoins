ec4291ef605725db072bc2b5dc953f5c9ab99967
AleksandarEnchev <guliat88@mail.bg>
2020-08-23 02:27:24 +0300

    Fix 'trades' migration (nullable)

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ Update 3 - 23.08.2020 - Split default welcome page to head, main, welcome <br />
 Update 4 - 23.08.2020 - Make 'Coins' (MVCMR) Model, Views, Controller, Migration, Routes <br />
 Update 5 - 23.08.2020 - Make 'Exchanges' (MVCMR) Model, Views, Controller, Migration, Routes <br />
 Update 6 - 23.08.2020 - Make 'Trades' (MVCMR) Model, Views, Controller, Migration, Routes <br />
-Update 7 - 23.08.2020 - 
+Update 7 - 
+
+---
+Fix 1 (UPD 6) - 23.08.2020 - Fix Trades migration -> nullable(open, close prices) <br />
--- database/migrations/2020_08_22_222904_create_trades_table.php
+++ database/migrations/2020_08_22_222904_create_trades_table.php
@@ -18,8 +18,8 @@ class CreateTradesTable extends Migration
             $table->unsignedTinyInteger('exchange_id');
             $table->unsignedTinyInteger('coin_id');
             $table->decimal('quantity', 16, 8);
-            $table->decimal('open_price', 13, 6);
-            $table->decimal('close_price', 13, 6);
+            $table->decimal('open_price', 13, 6)->nullable();
+            $table->decimal('close_price', 13, 6)->nullable();
             $table->unsignedTinyInteger('is_active')->default('1');
             $table->timestamps();
         });
