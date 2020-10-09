c7fa64b1c2a94e8a1655d90b607b421ac0e1e2ca
AleksandarEnchev <guliat88@mail.bg>
2020-10-09 12:59:30 +0300

    make updatePrices scheduled in Kernel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller {
         return view('welcome');
     }
 
-    public function updatePrices() {
+    static public function updatePrices() {
         $coins = Coins::all();
         $client = new CoinGeckoClient();
         foreach($coins as $coin) {
