dd10d6c9c6f4ea74b9d27a0230d01839743937c4
AleksandarEnchev <guliat88@mail.bg>
2020-11-10 14:10:24 +0200

    tradesSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,24 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
-    }
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		for ($i = 0; $i < 150; $i++) {
+			DB::table('trades')->insert([
+				'user_id' => 5,
+				'exchange_id' => rand(1, 5),
+				'coin_id' => rand(1, 20),
+				'quantity' => rand(0.3, 5000),
+				'open_price' => rand(0.015, 15000),
+			]);
+		}
+	}
 }
